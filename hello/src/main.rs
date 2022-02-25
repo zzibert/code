8899fd6c4048054b75f4c3de43d6f49ec71e884b
Žan Žibert <zzibert@plume.com>
2022-02-25 10:20:05 +0100

    Creating a reference to a large array

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,7 @@
-use std::convert::TryInto; // Enables to be called on those types that have implemented it (suck as u16)
-
 fn main() {
-  let a: i32 = 10;
-  let b: u16 = 100;
+  let a = 42;
+  let r = &a;
+  let b = a + *r;
 
-  let b_ = b.try_into().unwrap();
-
-  if a < b_ {
-    println!("Ten is less than one hundred.")
-  }
+  println!("a + a = {}", b);
 }
-
