9abc6889c29b040c74f404ac8b31ff26752abcc9
Žan Žibert <zzibert@plume.com>
2022-02-27 18:13:53 +0100

    Adding the GroundStation.send() and CubeSat.recv() methods

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,6 +4,20 @@ struct CubeSat {
   mailbox: Mailbox,
 }
 
+struct GroundStation;
+
+impl GroundStation {
+  fn send(&self, to: &mut CubeSat, msg: Message) {
+    to.mailbox.messages.push(msg);
+  }
+}
+
+impl CubeSat {
+  fn recv(&mut self) -> Option<Message> {
+    self.mailbox.messages.pop()
+  }
+}
+
 #[derive(Debug)]
 enum StatusMessage {
   Ok,
