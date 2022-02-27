648157c2949181e187bb018ac925b6849d1cc0eb
Žan Žibert <zzibert@plume.com>
2022-02-27 19:43:27 +0100

    Implementing the Copy trait manually

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,27 @@
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug)]
 struct CubeSat {
   id: u64,
 }
 
-#[derive(Debug,Copy,Clone)]
+#[derive(Debug)]
 enum StatusMessage {
   Ok,
+}
+
+impl Copy for CubeSat { }
+
+impl Copy for StatusMessage { }
+
+impl Clone for CubeSat {
+  fn clone(&self) -> Self {
+    CubeSat { id: self.id }
+  }
+}
+
+impl Clone for StatusMessage {
+  fn clone(&self) -> Self {
+    *self
+  }
 }
 
 fn check_status(sat_id: CubeSat) -> StatusMessage {
