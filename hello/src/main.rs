366537eb257e45caf8bb92ea9814dc1a889cf316
Žan Žibert <zzibert@plume.com>
2022-03-02 18:47:21 +0100

    Wrapping a user-defined type in Rc

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,41 +1,10 @@
-#[derive(Debug)]
-struct CubeSat {
-  id: u64,
-}
+use std::rc::Rc;
 
 #[derive(Debug)]
-enum StatusMessage {
-  Ok,
-}
-
-impl Copy for CubeSat { }
-
-impl Copy for StatusMessage { }
-
-impl Clone for CubeSat {
-  fn clone(&self) -> Self {
-    CubeSat { id: self.id }
-  }
-}
-
-impl Clone for StatusMessage {
-  fn clone(&self) -> Self {
-    *self
-  }
-}
-
-fn check_status(sat_id: CubeSat) -> StatusMessage {
-  StatusMessage::Ok
-}
+struct GroundStation {}
 
 fn main() {
-  let sat_a = CubeSat { id: 0 };
+  let base = Rc::new(GroundStation {});
 
-  let a_status = check_status(sat_a.clone());
-
-  println!("a {:?}", a_status.clone());
-
-  let a_status = check_status(sat_a);
-
-  println!("a {:?}", a_status);
+  println!("{:?}", base);
 }
