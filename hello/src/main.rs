1c936802ad62bc82a14347a08fd6880d428ac5f1
Žan Žibert <zzibert@plume.com>
2022-03-02 20:32:01 +0100

    Isolating and decoding the exponent from an f32

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
   let n: f32 = 42.42;
   let n_bits: u32 = n.to_bits();
-  let sign_bit = n_bits >> 31;
-
-  println!("{:?}", sign_bit)
+  let exponent_ = n_bits >> 23;
+  let exponent_ = exponent_ & 0xff;
+  let exponent = (exponent_ as i32) - 127;
+  println!("{:?}", exponent)
 }
