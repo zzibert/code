5640092b69bfcb2680c495d67afdd00ccf8bec6d
Žan Žibert <zzibert@plume.com>
2022-03-05 11:27:14 +0100

    Creating a raw pointer

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-  let n: f32 = 42.42;
-  let n_bits: u32 = n.to_bits();
-  let exponent_ = n_bits >> 23;
-  let exponent_ = exponent_ & 0xff;
-  let exponent = (exponent_ as i32) - 127;
-  println!("{:?}", exponent)
+  let a: i64 = 42;
+  let a_ptr = &a as *const i64;
+
+  println!("a: {} ({:p})", a, a_ptr);
 }
