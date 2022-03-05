f6e59248e719db7e284b86f5797a83223aef0ab4
Žan Žibert <zzibert@plume.com>
2022-03-05 11:37:31 +0100

    Identifying a values address

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
   let a: i64 = 42;
   let a_ptr = &a as *const i64;
+  let a_addr: usize = unsafe {
+    std::mem::transmute(a_ptr)
+  };
 
-  println!("a: {} ({:p})", a, a_ptr);
+  println!("a: {} ({:p})...0x{:x}", a, a_ptr, a_addr + 7)
 }
