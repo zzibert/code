d0ac5921194b1981ce62ba2f127792bbd33c4212
Žan Žibert <zzibert@plume.com>
2022-03-05 17:27:16 +0100

    Allocating and deallocationg memory on the heap via Box<T>

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,15 @@
-fn main() {
-  let a: i32 = 40;
-  let b: Box<i32> = Box::new(60);
+use std::mem::drop;
 
-  println!("{} + {} = {}", a, b, a + *b);
+fn main() {
+  let a = Box::new(1);
+  let b = Box::new(1);
+  let c = Box::new(1);
+
+  let result1 = *a + *b + *c;
+
+  drop(a);
+  let d = Box::new(1);
+  let result2 = *b + *c + *d;
+
+  println!("{} {}", result1, result2);
 }
